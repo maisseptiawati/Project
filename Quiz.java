43e23493b4b5f44efd96e4138b63ea5ae098a4e1
maisseptiawati <septiawatimais@gmail.com>
2016-04-11 11:43:53 +0700

    Project

---
+++ Quiz.java
@@ -0,0 +1,374 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+/**
+ *
+ * @author Ma'is
+ */
+public class Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Kerjakan soal berikut dengan benar !!");
+       System.out.println("soal pilihan ganda"); 
+       String[] a = {"Tsakafah Bani Saidah","Madinah", "Mekkah","Indoesia"};
+       System.out.println("1. Tempat pertemuan Muhajirin dan Anshar ...");
+       System.out.println("a." + a[0]);
+       System.out.println("b." + a[1]);
+       System.out.println("c." + a[2]);
+       System.out.println("d." + a[3]);
+       
+       String[] b = {"Hafinah","Hanafi", "Hanifah","Hafani"};
+       System.out.println("2. Musailamah Al-Kazzab adalah nabi palsu dari suku ? ...");
+       System.out.println("a." + b[0]);
+       System.out.println("b." + b[1]);
+       System.out.println("c." + b[2]);
+       System.out.println("d." + b[3]);
+       
+       String[] c = {"10","50", "100","70"};
+       System.out.println("3. berapa orang penghafal Al-Quran yang gugur dalam perang Yamamah ?...");
+       System.out.println("a." + c[0]);
+       System.out.println("b." + c[1]);
+       System.out.println("c." + c[2]);
+       System.out.println("d." + c[3]);
+       
+       String[] d = {"Muhajirin","Muhajiroh", "Muhajirna","Muhajaja"};
+       System.out.println("4. Kelompok masyarakat muslim Makkah yang hijrah ke Madinnah disebut ?...");
+       System.out.println("a." + d[0]);
+       System.out.println("b." + d[1]);
+       System.out.println("c." + d[2]);
+       System.out.println("d." + d[3]);
+       
+       String[] e = {"Khalifah","Khafilah", "Khafalah","Khafilin"};
+       System.out.println("5. Kepala pemerintah pengganti Rasulullah disebut ? ...");
+       System.out.println("a." + e[0]);
+       System.out.println("b." + e[1]);
+       System.out.println("c." + e[2]);
+       System.out.println("d." + e[3]);
+       
+       String[] f = {"1 Jumadil Awal 13 H","5 Jumadil Awal 13 H", "6 Jumadil Awal 13 H","8 Jumadil Awal 13 H"};
+       System.out.println("6. Khalifah Abu Bakar As-Sidiq meninggal pada ? ...");
+       System.out.println("a." + f[0]);
+       System.out.println("b." + f[1]);
+       System.out.println("c." + f[2]);
+       System.out.println("d." + f[3]);
+       
+       String[] g = {"10 tahun","11 tahun", "111 tahun","110 tahun"};
+       System.out.println("7. Berapa tahun Utsman menjabat sebagai Khalifah ? ...");
+       System.out.println("a." + g[0]);
+       System.out.println("b." + g[1]);
+       System.out.println("c." + g[2]);
+       System.out.println("d." + g[3]);
+       
+       String[] h = {"Amir Bin Ash","Amru Bin Ash", "Amar Bin Ash","Amr Bin Ash"};
+       System.out.println("8. Mesir dapat di tundukan oleh pasukan Islam yang dipimpin oleh ? ...");
+       System.out.println("a." + h[0]);
+       System.out.println("b." + h[1]);
+       System.out.println("c." + h[2]);
+       System.out.println("d." + h[3]);
+       
+       String[] i = {"Abid Lu'lu","Abu Lu'lu", "Abud Lu'lu","Abdul Lu'lu"};
+       System.out.println("9. Khalifah Umar Bin Khattab dibunuh oleh ? ...");
+       System.out.println("a." + i[0]);
+       System.out.println("b." + i[1]);
+       System.out.println("c." + i[2]);
+       System.out.println("d." + i[3]);
+       
+       String[] j = {"Jalal","Jalil", "Jamal","Janal"};
+       System.out.println("10. Perang antara Ali dengan Siti Aisyah disebut ? ...");
+       System.out.println("a." + j[0]);
+       System.out.println("b." + j[1]);
+       System.out.println("c." + j[2]);
+       System.out.println("d." + j[3]);
+       
+       String[] k = {"Al-Fatihah","Al-Baqarah", "Ali Imran","Al-Fil"};
+       System.out.println("11. Surat Al-Qur'an yang menceritakan pasukan gajah adalah  ? ...");
+       System.out.println("a." + k[0]);
+       System.out.println("b." + k[1]);
+       System.out.println("c." + k[2]);
+       System.out.println("d." + k[3]);
+       
+       String[] l = {"Jabariyyah","Jahiliyah", "Jalalaiyah","Jalaluddin"};
+       System.out.println("12. Jaman kebodohan disebut ? ...");
+       System.out.println("a." + l[0]);
+       System.out.println("b." + l[1]);
+       System.out.println("c." + l[2]);
+       System.out.println("d." + l[3]);
+       
+       String[] m = {"Abid Lu'lu","Abu Lu'lu", "Abud Lu'lu","Abdul Lu'lu"};
+       System.out.println("13. Khalifah Umar Bin Khattab dibunuh oleh ? ...");
+       System.out.println("a." + m[0]);
+       System.out.println("b." + m[1]);
+       System.out.println("c." + m[2]);
+       System.out.println("d." + m[3]);
+       
+       String[] n = {"Bertengkar","Berselisih", "Perang","Damai"};
+       System.out.println("14. Masa perjanjian hudaibiyyah disebut masa ? ...");
+       System.out.println("a." + n[0]);
+       System.out.println("b." + n[1]);
+       System.out.println("c." + n[2]);
+       System.out.println("d." + n[3]);
+       
+       String[] o = {"10.000 tentara","7.000 tentara", "5.000 tentara","1.000"};
+       System.out.println("15. Jumlah pasukan kaum muslimin saat menaklukan kota Mekkah adalah ? ...");
+       System.out.println("a." + o[0]);
+       System.out.println("b." + o[1]);
+       System.out.println("c." + o[2]);
+       System.out.println("d." + o[3]);
+       
+       String[] p = {"2 H","5 H", "8 H","10 H"};
+       System.out.println("16. Pasukan Islam meninggalkan Marru Zahran menuju Mekkah terjadi pada tahun ? ...");
+       System.out.println("a." + p[0]);
+       System.out.println("b." + p[1]);
+       System.out.println("c." + p[2]);
+       System.out.println("d." + p[3]);
+       
+       String[] q = {"116-120","111-112", "110-111","117-118"};
+       System.out.println("17. Surat At-Taubah ayat berapakah yang diturunkan pada saat Perang Tabuk? ...");
+       System.out.println("a." + q[0]);
+       System.out.println("b." + q[1]);
+       System.out.println("c." + q[2]);
+       System.out.println("d." + q[3]);
+       
+       String[] r = {"25 Zulkaidah","16 Zulkaidah", "20 Zulkaidah","15 Zulkaidah"};
+       System.out.println("18. Nabi melakukan Haji Wada' pada tanggal ? ...");
+       System.out.println("a." + r[0]);
+       System.out.println("b." + r[1]);
+       System.out.println("c." + r[2]);
+       System.out.println("d." + r[3]);
+       
+       String[] s = {"Bani Khuza'ah","Bani Bakar", "Bani Sulaim","Bani Hasyim"};
+       System.out.println("19. Jumlah pada perang Hunainan  ? ...");
+       System.out.println("a." + s[0]);
+       System.out.println("b." + s[1]);
+       System.out.println("c." + s[2]);
+       System.out.println("d." + s[3]);
+       
+       String[] t = {"20000 Kaum Muslimin","80000 Kaum Muslimin", "100000 Kaum Muslimin","200000 Kaum Muslimin"};
+       System.out.println("20. Berapa jumlah kaum muslimin yang mengikuti Haji Wada ? ...");
+       System.out.println("a." + t[0]);
+       System.out.println("b." + t[1]);
+       System.out.println("c." + t[2]);
+       System.out.println("d." + t[3]);
+       
+        String[] u = {"Hura-hura","Kebodohan", "Kematian","Duka Cita"};
+       System.out.println("21. Zaman Jahiliyyah disebut  ? ...");
+       System.out.println("a." + u[0]);
+       System.out.println("b." + u[1]);
+       System.out.println("c." + u[2]);
+       System.out.println("d." + u[3]);
+       
+        String[] v = {"Baitul Aqabah","Piagam Madinah", "Perjanjian Hudaibiyyah","Baitullah"};
+       System.out.println("22. Janji setia oenduduk Madinah untuk membela Nabi Muhammad disebut ? ...");
+       System.out.println("a." + v[0]);
+       System.out.println("b." + v[1]);
+       System.out.println("c." + v[2]);
+       System.out.println("d." + v[3]);
+       
+       String[] w = {"75 Orang Madinah","12 Orang Madinah", "600 Orang Muslimin","50 Orang Muslimin"};
+       System.out.println("23. Baitul Aqabah kedua diikuti oleh ? ...");
+       System.out.println("a." + t[0]);
+       System.out.println("b." + t[1]);
+       System.out.println("c." + t[2]);
+       System.out.println("d." + t[3]);
+       
+       String[] x = {"Abu Bakar Siddiq","Ali Bin Abi Thalib", "Abu Salamah","Zaid Bin Harits"};
+       System.out.println("24. Sahabat Nabi yang pertama berangkat ke Madinah adalah ? ...");
+       System.out.println("a." + x[0]);
+       System.out.println("b." + x[1]);
+       System.out.println("c." + x[2]);
+       System.out.println("d." + x[3]);
+       
+       String[] y = {"Gua Tsur","Gua Hior", "Jabal Nur","Jabal Rahmah"};
+       System.out.println("25. Tempat persembunyian Nabi dari kejaran Kaum Kafir ? ...");
+       System.out.println("a." + y[0]);
+       System.out.println("b." + y[1]);
+       System.out.println("c." + y[2]);
+       System.out.println("d." + y[3]);
+       
+       String[] z = {"Abu Bakar As-Sidiq","Ali Bin Abi Thalib", "Umar Bin Khattab","Zaid bin Haritsh"};
+       System.out.println("26. Sahabat Nabi yang setia dalam Hijrah adalah ? ...");
+       System.out.println("a." + z[0]);
+       System.out.println("b." + z[1]);
+       System.out.println("c." + z[2]);
+       System.out.println("d." + z[3]);
+       
+       String[] aa = {"Menjungjung","Mengingkari", "Menghargai","Mematuhi"};
+       System.out.println("27. Terhadap perjanjian Hudaibiyyah orang Quraisy bersikap ? ...");
+       System.out.println("a." + aa[0]);
+       System.out.println("b." + aa[1]);
+       System.out.println("c." + aa[2]);
+       System.out.println("d." + aa[3]);
+       
+       String[] bb = {"Raja Gassanac","Raja Yaman", "Raja Abbeseniad","Raja Oman"};
+       System.out.println("28. Raja yang membunuh dan menolak utusan Nabi adalah ? ...");
+       System.out.println("a." + bb[0]);
+       System.out.println("b." + bb[1]);
+       System.out.println("c." + bb[2]);
+       System.out.println("d." + bb[3]);
+       
+       String[] cc = {"Menang","Gugur", "Kalah","Di ba'iat"};
+       System.out.println("29. Dalam perang Mut'ah Zaid Bin Haritsh akhirnya  ? ...");
+       System.out.println("a." + cc[0]);
+       System.out.println("b." + cc[1]);
+       System.out.println("c." + cc[2]);
+       System.out.println("d." + cc[3]);
+       
+       String[] dd = {"Madinah","Mekkah", "Perkemahan","Persembunyian"};
+       System.out.println("30. Pada Perang Mut'ah pasukan islam di tarik mundur ke ? ...");
+       System.out.println("a." + dd[0]);
+       System.out.println("b." + dd[1]);
+       System.out.println("c." + dd[2]);
+       System.out.println("d." + dd[3]);
+       
+       String[] ee = {"Pengingkaran Perjanjian Hudaibiyyah","Kaum Quraisy balas dendam", "Kaum Quraisy dan Kaum Muslimin","Kaum muslimin membenci kaum Quraisy"};
+       System.out.println("31. Fathu Mekkah terjadi karena ? ...");
+       System.out.println("a." + ee[0]);
+       System.out.println("b." + ee[1]);
+       System.out.println("c." + ee[2]);
+       System.out.println("d." + ee[3]);
+       
+       String[] ff = {"Bani Qoinuqoc","Bani Nadar", "Bani Bakard","Bani Khuza'ah"};
+       System.out.println("32. Suku yang berhasil masuk Islam adalah Suku ? ...");
+       System.out.println("a." + ff[0]);
+       System.out.println("b." + ff[1]);
+       System.out.println("c." + ff[2]);
+       System.out.println("d." + ff[3]);
+       
+       String[] gg = {"Menyerang","Memperbaiki Perjanjian", "Berperang","Berdiskusi"};
+       System.out.println("33. Abu Sufyan dikirim menemui Rasulullah untuk ? ...");
+       System.out.println("a." + gg[0]);
+       System.out.println("b." + gg[1]);
+       System.out.println("c." + gg[2]);
+       System.out.println("d." + gg[3]);
+       
+       String[] hh = {"Zakat","Shalat", "Menyembah Nabi","Menutup pintu rumahnya"};
+       System.out.println("34. Kaum Quraisy dianggap aman apabila ? ...");
+       System.out.println("a." + hh[0]);
+       System.out.println("b." + hh[1]);
+       System.out.println("c." + hh[2]);
+       System.out.println("d." + hh[3]);
+       
+       String[] ii = {"Khandak","Hunain", "Tabuk","Uhud"};
+       System.out.println("35. Perang terakhir yang diikut Nabi ? ...");
+       System.out.println("a." + ii[0]);
+       System.out.println("b." + ii[1]);
+       System.out.println("c." + ii[2]);
+       System.out.println("d." + ii[3]);
+       
+       String[] jj = {"Khutbah Haji","Khutbah Wadak", "Khutbah Jum'at","Khutbah Wajib"};
+       System.out.println("36. Khutbah terakhir yang dilakukan Nabi ketika Haji disebut ? ...");
+       System.out.println("a." + jj[0]);
+       System.out.println("b." + jj[1]);
+       System.out.println("c." + jj[2]);
+       System.out.println("d." + jj[3]);
+       
+       String[] kk = {"Masjidil Haram","Aarafah", "Mina","Safa-Marwah"};
+       System.out.println("37. Tempat pertama yang di datangi Rasulullah sesampainya datang di Mekkah  ? ...");
+       System.out.println("a." + kk[0]);
+       System.out.println("b." + kk[1]);
+       System.out.println("c." + kk[2]);
+       System.out.println("d." + kk[3]);
+       
+       String[] ll = {"Zainab","Khadijah", "Hindun","Aisyah"};
+       System.out.println("38. Istri Nabi yang merupakan putri Abu Bakar As-Siddiq adalah ? ...");
+       System.out.println("a." + ll[0]);
+       System.out.println("b." + ll[1]);
+       System.out.println("c." + ll[2]);
+       System.out.println("d." + ll[3]);
+       
+       String[] mm = {"8 Juni 630 M","8 Juni 631 M", "8 Juni 632 M","8 Juni 633 M"};
+       System.out.println("39. Wafat Nabi pada tanggal ? ...");
+       System.out.println("a." + mm[0]);
+       System.out.println("b." + mm[1]);
+       System.out.println("c." + mm[2]);
+       System.out.println("d." + mm[3]);
+       
+       String[] nn = {"23 Tahun","25 Tahun", "24 Tahun","26 Tahun"};
+       System.out.println("40. Nabi dakwah selama  ? ...");
+       System.out.println("a." + nn[0]);
+       System.out.println("b." + nn[1]);
+       System.out.println("c." + nn[2]);
+       System.out.println("d." + nn[3]);
+       
+       String[] oo = {"Fir'aun","Obama", "Abraham","Abrahah"};
+       System.out.println("41. Raja Negeri Yaman yang ingin menghancurkan Ka'bah adalah ? ...");
+       System.out.println("a." + oo[0]);
+       System.out.println("b." + oo[1]);
+       System.out.println("c." + oo[2]);
+       System.out.println("d." + oo[3]);
+       
+       String[] pp = {"10 Orang","12 Orang", "60 Orang","75 Orang"};
+       System.out.println("42. Jumlah kaum muslimin yang ikut dalam baiat pertama ? ...");
+       System.out.println("a." + pp[0]);
+       System.out.println("b." + pp[1]);
+       System.out.println("c." + pp[2]);
+       System.out.println("d." + pp[3]);
+       
+       String[] qq = {"Khutbah Haji","Khutbah Wadak", "Khutbah Jum'at","Khutbah Wajib"};
+       System.out.println("43. Khutbah terakhir yang dilakukan Nabi ketika Haji disebut ? ...");
+       System.out.println("a." + qq[0]);
+       System.out.println("b." + qq[1]);
+       System.out.println("c." + qq[2]);
+       System.out.println("d." + qq[3]);
+       
+       String[] rr = {"Aisyah binti Abu Bakar","Ummu Kulsum", "Fatimah binti Muhammad","Afra binti Ubaid"};
+       System.out.println("44. Seorang wanita yang ikut pada baiat pertama ? ...");
+       System.out.println("a." + rr[0]);
+       System.out.println("b." + rr[1]);
+       System.out.println("c." + rr[2]);
+       System.out.println("d." + rr[3]);
+       
+       String[] ss = {"Ar-Rijal","Kubra", "An-Nisa","Sugra"};
+       System.out.println("45. Baiat pertama dikenal dengan ? ...");
+       System.out.println("a." + ss[0]);
+       System.out.println("b." + ss[1]);
+       System.out.println("c." + ss[2]);
+       System.out.println("d." + ss[3]);
+       
+       String[] tt = {"Darus Salam","Darun Naim", "Darul Muttaqin","Darun Nadwah"};
+       System.out.println("46. Tempat bermusyawarah suku Quraisy adalah ? ...");
+       System.out.println("a." + tt[0]);
+       System.out.println("b." + tt[1]);
+       System.out.println("c." + tt[2]);
+       System.out.println("d." + tt[3]);
+       
+       String[] uu = {"Persaudaraan","orang yang berhijrah", "Penolong","Orang ynag kembali"};
+       System.out.println("47. Arti dari Muhajirin ? ...");
+       System.out.println("a." + uu[0]);
+       System.out.println("b." + uu[1]);
+       System.out.println("c." + uu[2]);
+       System.out.println("d." + uu[3]);
+       
+       String[] vv = {"Persaudaraan","orang yang berhijrah", "Penolong","Orang ynag kembali"};
+       System.out.println("48. Apa arti dari Anshar ? ...");
+       System.out.println("a." + vv[0]);
+       System.out.println("b." + vv[1]);
+       System.out.println("c." + vv[2]);
+       System.out.println("d." + vv[3]);
+       
+       String[] ww = {"Ternak","Tani", "Dagang","Pegawai"};
+       System.out.println("49. Mata pencarian penduduk madinah ? ...");
+       System.out.println("a." + ww[0]);
+       System.out.println("b." + ww[1]);
+       System.out.println("c." + ww[2]);
+       System.out.println("d." + ww[3]);
+       
+       String[] xx = {" Masjid Nabawi"," Masjid Aqsa", "Masjid Haram","Masjid Kuba"};
+       System.out.println("50. Masjid yang pertama kali di dirikan ? ...");
+       System.out.println("a." + xx[0]);
+       System.out.println("b." + xx[1]);
+       System.out.println("c." + xx[2]);
+       System.out.println("d." + xx[3]);
+       
+       
+       
+    }
+    
+}
